b80457f0f13c6fd74f6235e817f7b18bc77445be
xerxii <r-limpz@users.noreply.github.com>
2025-10-05 01:23:38 +0800

    interface fix sizes

--- src/components/cards/CardsOffers.vue
+++ src/components/cards/CardsOffers.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex justify-center space-x-10">
         <div v-for="offer in offers"
-            class="w-64 h-96 border border-gray-500 rounded-lg shadow-sm hover:border-cyan-300 transition delay-150 duration-300 ease-in-out">
-            <img :src="offer.image" alt="" class="h-48 w-full rounded-t-lg object-cover"></img>
+            class="w-64 h-96 max-h-[350px] border border-gray-500 rounded-lg shadow-sm hover:border-cyan-300 transition delay-150 duration-300 ease-in-out">
+            <img :src="offer.image" alt="" class="h-48 w-auto rounded-t-lg object-cover"></img>
             <div class="space-y-8 p-4">
                 <div class="">
                     <h5 class=" text-md font-bold text-center"> {{ offer.service }} </h5>
